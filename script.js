5a4a449c94fe444150958992e3ece4a2c44e25fa
dora <D1249653@o365.fcu.edu.tw>
2025-05-07 00:45:10 +0800

    改

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function togglePassword() {
     const passwordInput = document.getElementById('password');
-    const button = event.currentTarget;
     if (passwordInput.type === 'password') {
         passwordInput.type = 'text';
     } else {
