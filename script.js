37a40bfc643d31b36fc7cd2e44fe91537f4e2412
dora <D1249653@o365.fcu.edu.tw>
2025-05-07 03:58:43 +0800

    最後改

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function togglePassword() {
         passwordInput.type = 'password';
     }
 }
+
+function changeLanguage(lang) {
+    if (lang === 'zh_tw') {
+        document.getElementById('username').placeholder = '帳號';
+        document.getElementById('password').placeholder = '密碼';
+        document.querySelector('.login-button').innerText = '登入';
+        document.querySelector('.cookie-button').innerText = 'Cookie使用資訊';
+    } else if (lang === 'en') {
+        document.getElementById('username').placeholder = 'Username';
+        document.getElementById('password').placeholder = 'Password';
+        document.querySelector('.login-button').innerText = 'Log in';
+        document.querySelector('.cookie-button').innerText = 'Cookie notice';
+    }
+}
